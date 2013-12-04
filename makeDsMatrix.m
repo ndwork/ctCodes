456bc331ff62a2a5cc743e64a2e7e0b5ab6ccc1b
Thomas Walther <thomaswa@ccrma.stanford.edu>
2013-12-03 18:15:03 -0800

    Main contribution is a fixed (we think) dsTransform.m and makeDsMatrix.m. marDS.m doesn't work.

--- makeDsMatrix.m
+++ makeDsMatrix.m
@@ -6,13 +6,28 @@ function dsMatrix = makeDsMatrix( A, phi, x, y )
   nA = numel(A);
   nPhi = numel(phi);
 
-  dsMatrix = sparse(nA*nPhi,Nx*Ny)
+  nNonzero = Ny * nA * nPhi;
+  colIndxs = zeros( nNonzero, 1 );
+  rowIndxs = zeros( nNonzero, 1 );
+  values   = zeros( nNonzero, 1 );
 
-  parfor i=1:Nx*Ny
-    dsIn = zeros(Ny,Nx);
+  rowImg = (1:Ny)' * ones(1,Nx);
+  colImg = ones(Ny,1) * (1:Nx);
+
+  for i=1:nA*nPhi
+    if mod(i,100)==0
+      disp(['dsMatrix ', num2str(i), ' of ', num2str(nA*nPhi)]);
+    end
+    dsIn = zeros(nA,nPhi);
     dsIn(i) = 1;
     dsOut = dsTransform( dsIn, A, phi, x, y );
-    dsMatrix(:,i) = dsOut(:);
+    nonzeroIndxs = find( dsOut ~= 0 );
+    colIndxs( (i-1)*Ny + 1 : i*Ny ) = colImg( nonzeroIndxs );
+    rowIndxs( (i-1)*Ny + 1 : i*Ny ) = rowImg( nonzeroIndxs );
+    values( (i-1)*Ny + 1 : i*Ny ) = dsOut( nonzeroIndxs );
   end
 
+  dsMatrix = sparse( rowIndxs, colIndxs, values );
 end
+
+
